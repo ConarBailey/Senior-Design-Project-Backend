c05025eaaa8eecee832e26763b031ac12330b55b
Lauraluu04 <lml9874@mavs.uta.edu>
2025-11-29 15:16:14 -0600

    Update root.js

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/{index{.html}}', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'Home.html'));
+});
+router.get('/{Home{.html}}', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'Home.html'));
+});
+router.get('/{login{.html}}', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'));
+});
+router.get('/{register{.html}}', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'register.html'));
 });
 
 module.exports = router;
