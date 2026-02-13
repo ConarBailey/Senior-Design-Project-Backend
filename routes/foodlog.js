86ef3ac2afa6e3c2c48e44129523a9708de30994
Lauraluu04 <lml9874@mavs.uta.edu>
2026-02-12 21:15:04 -0600

    Update foodlog.js

--- routes/foodlog.js
+++ routes/foodlog.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const foodlogController = require('../controllers/foodlogController');
 
-router.post('/', foodlogController.handleNewFoodLog);
+router.route('/')
+    .get(foodlogController.getUsersLog)
+    .post(foodlogController.handleNewFoodLog)
+    .put(foodlogController.updateFoodLog)
+    .delete(foodlogController.deleteFoodLog);
+
+router.route('/:username')
+    .get(foodlogController.getUserLog);
 
 module.exports = router;
